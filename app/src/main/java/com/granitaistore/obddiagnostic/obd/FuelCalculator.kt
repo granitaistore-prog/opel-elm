853a5122cfa761e1a12b15293895c9b53e39d94e
granitaistore-prog <granit.ai.store@gmail.com>
2026-01-06 11:44:27 +0200

    Create FuelCalculator.kt

---
+++ app/src/main/java/com/granitaistore/obddiagnostic/obd/FuelCalculator.kt
@@ -0,0 +1,12 @@
+package com.granitaistore.obddiagnostic.obd
+
+object FuelCalculator {
+
+    fun fuelLph(maf: Double): Double {
+        return 0.0
+    }
+
+    fun fuelL100km(speed: Double, lph: Double): Double {
+        return 0.0
+    }
+}
