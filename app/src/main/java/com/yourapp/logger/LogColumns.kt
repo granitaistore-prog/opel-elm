5023a44039bddb1b45e65af5f56a5004fdd64844
granitaistore-prog <granit.ai.store@gmail.com>
2026-01-08 12:54:56 +0200

    1

---
+++ app/src/main/java/com/yourapp/logger/LogColumns.kt
@@ -0,0 +1,16 @@
+package com.yourapp.logger
+
+object LogColumns {
+    val HEADER = listOf(
+        "Time",
+        "RPM",
+        "Speed",
+        "CoolantTemp",
+        "IntakeTemp",
+        "Boost",
+        "Throttle",
+        "Voltage",
+        "CAN_ID",
+        "CAN_DATA"
+    )
+}
